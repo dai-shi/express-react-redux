8db58109893a3453521b34b8649be46c62dacbcc
daishi <daishi@axlight.com>
2016-11-01 06:30:59 +0900

    files for test

---
+++ tests/files/src/client/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <h1>Hello</h1>
+  </div>
+);
---
+++ tests/files/src/client/index.js
@@ -0,0 +1,8 @@
+/* eslint-env browser */
+
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './app';
+
+render(React.createElement(App), document.getElementById('app'));
