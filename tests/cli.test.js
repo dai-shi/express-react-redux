6c3727d5ce68615793720df2c15f82b3020975ff
daishi <daishi@axlight.com>
2017-01-20 07:32:14 +0900

    fix cli test

--- tests/cli.test.js
+++ tests/cli.test.js
@@ -2,25 +2,35 @@
 
 const path = require('path');
 const fs = require('fs-extra');
-const exec = require('child_process').exec;
+const execSync = require('child_process').execSync;
 
 const cli = path.join(__dirname, '../lib/cli.js');
 
 describe('cli import test', () => {
-  let cwd;
+  const encoding = 'utf8';
+  let dir;
   beforeEach(() => {
-    cwd = fs.mkdtempSync('./temp-');
+    dir = fs.mkdtempSync('./temp-');
   });
   afterEach(() => {
-    fs.removeSync(cwd);
+    fs.removeSync(dir);
   });
-  it('call it without commands', (done) => {
-    exec(cli, { cwd }, (error, stdout, stderr) => {
-      expect(error).toBeFalsy();
-      expect(stderr).toBeFalsy();
-      expect(stdout.indexOf('Usage') >= 0).toBe(true);
-      done();
-    });
+
+  it('invoke without commands', () => {
+    const stdout = execSync(`${cli} --help`, { cwd: dir, encoding });
+    expect(stdout.indexOf('Usage') >= 0).toBe(true);
+  });
+
+  it('invoke import tiny-todos', () => {
+    const cwd = path.join(dir, 'app');
+    fs.mkdirSync(cwd);
+    execSync('npm init -y', { cwd });
+    const stdout = execSync(`${cli} import tiny-todos`, { cwd, encoding });
+    expect(stdout.length).toBe(0);
+    const pjson = fs.readFileSync(path.join(cwd, 'package.json'), { encoding });
+    expect(pjson.indexOf('express-react-redux build-client') >= 0).toBe(true);
+    const appjs = fs.readFileSync(path.join(cwd, 'src/server/app.js'), { encoding });
+    expect(appjs.indexOf('app.use(require(\'express-react-redux\')());') >= 0).toBe(true);
   });
 });
 
