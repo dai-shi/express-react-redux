9e87c896a42561d4041ba9737f1f3e514df2ea98
Gionatan Lombardi <gionatan@vangogh-creative.it>
2017-09-11 16:32:15 +0200

    Optional async function to populate the Store before Server Side Rendering

--- tests/files/src/client/reducer.js
+++ tests/files/src/client/reducer.js
@@ -1,1 +1,9 @@
-export default () => ({});
+export default (state = { todos: [] }, action) => {
+  if (action.type === 'ADD_TODO') {
+    return {
+      ...state,
+      todos: [...state.todos, action.todo],
+    };
+  }
+  return state;
+};
--- tests/middleware.test.js
+++ tests/middleware.test.js
@@ -9,7 +9,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const main = require('../lib/main.js');
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 2 * 60 * 1000;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 3 * 60 * 1000;
 
 const webpackDevConfig = require('../lib/webpack-dev-config.js');
 
@@ -81,6 +81,41 @@ describe('middleware run test with / route SSR', () => {
   it('get /', (done) => {
     request.get(`http://localhost:${port}/`, (err, res, body) => {
       expect(body).toContain('<h1');
+      done();
+    });
+  });
+
+  afterAll((done) => {
+    server.close(done);
+  });
+});
+
+describe('middleware run test with async function to populate the Store before SSR', () => {
+  'use strict';
+
+  let server;
+  let port;
+  beforeAll((done) => {
+    const app = express();
+    app.use(main({
+      webpackDevConfig,
+      webpackDevBuildCallback: () => done(),
+      indexSSR: true,
+      beforeRenderPromise: (() => new Promise((resolve) => {
+        setTimeout(() => {
+          resolve({ type: 'ADD_TODO', todo: 'Wait async state before render 👏 ' });
+        }, 2000);
+      }))(),
+    }));
+    server = http.createServer(app);
+    server.listen(() => {
+      port = server.address().port;
+    });
+  });
+
+  it('get /', (done) => {
+    request.get(`http://localhost:${port}/`, (err, res, body) => {
+      expect(body).toContain('Wait async state before render 👏');
       done();
     });
   });
